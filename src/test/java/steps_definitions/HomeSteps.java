02b043fe1448ba8a180f9c99bee5a42ef8d84523
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-06-07 22:03:10 -0400

    added task 1 solution

--- src/test/java/steps_definitions/HomeSteps.java
+++ src/test/java/steps_definitions/HomeSteps.java
@@ -1,4 +1,10 @@
 package steps_definitions;
 
+import io.cucumber.java.en.Then;
+
 public class HomeSteps {
+    @Then("Verify all links are displayed")
+    public void verify_all_links_are_displayed() {
+        System.out.println("verified all links are displayed");
+    }
 }
--- src/test/resources/features/home.feature
+++ src/test/resources/features/home.feature
@@ -2,4 +2,6 @@ Feature: Home page related scenarios
 
   Scenario: Verify header of the page is Automation with Selenium
 
-
+  Scenario: Verify all links are displayed
+    Given User navigates to www.automation.techleadacademy.io
+    Then Verify all links are displayed
