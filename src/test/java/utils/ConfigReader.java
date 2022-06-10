2988c2cfb6efbc92b098369d4895fd3092db4c1a
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-06-09 20:28:17 -0400

    modified readProperty() method, removed path argument

--- src/test/java/utils/ConfigReader.java
+++ src/test/java/utils/ConfigReader.java
@@ -6,10 +6,10 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
-    public static String readProperty(String filePath, String property){
+    public static String readProperty(String property){
         Properties prop = null;
         try{
-            FileInputStream fileInput = new FileInputStream(filePath);
+            FileInputStream fileInput = new FileInputStream("config.properties");
             prop = new Properties();
             prop.load(fileInput);
         }catch (FileNotFoundException e){
